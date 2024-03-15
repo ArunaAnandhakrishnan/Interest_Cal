dcbde40d43d393d303bbc70da76f7c54cefac377
Aruna ravichandran <V00554@VERINITE1.local>
2024-03-15 16:17:12 +0530

    minor changes done

--- Mad_Calculator/src/main/java/com/vernite/cal/repository/ProfilesRepository.java
+++ Mad_Calculator/src/main/java/com/vernite/cal/repository/ProfilesRepository.java
@@ -7,10 +7,4 @@ import com.vernite.cal.model.Profiles;
 import java.util.Optional;
 
 public interface ProfilesRepository extends JpaRepository<Profiles, Long> {
-
-	public Profiles findBySerno(Long serno);
-
-	public Profiles findByDescription(String description);
-
-	Optional<Profiles> findByStmtserno();
 }
