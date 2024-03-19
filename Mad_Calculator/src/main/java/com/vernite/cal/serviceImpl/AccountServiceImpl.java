4c5f516b62e846e4ab5b57370bf9596b30fa158e
Aruna ravichandran <V00554@VERINITE1.local>
2024-03-19 15:33:57 +0530

    date format changed for cycle date in response

--- Mad_Calculator/src/main/java/com/vernite/cal/serviceImpl/AccountServiceImpl.java
+++ Mad_Calculator/src/main/java/com/vernite/cal/serviceImpl/AccountServiceImpl.java
@@ -92,25 +92,43 @@ public class AccountServiceImpl implements AccountService {
 
 		Optional<Cstmtsettings> cstmtSettingsData = cstatementSettingsRepository.findByProfiles(profiles);
 		Long minpaypercentage = cstmtSettingsData.get().getMinpaypercentage();
+//		List<String> cycledate = new ArrayList<>();
+//		List<Cstatements> cstatementsList = caccounts.getCstatementsList();
+//		DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+//		for (Cstatements statements : cstatementsList) {
+//	        LocalDateTime localDateTime = statements.getCycledate().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+//			LocalDate localDate = localDateTime.toLocalDate();
+//			String outputDateStr = outputFormatter.format(localDate);
+//
+//			cycledate.add(outputDateStr);
+//		}
+
 		List<String> cycledate = new ArrayList<>();
 		List<Cstatements> cstatementsList = caccounts.getCstatementsList();
 		DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+		Comparator<String> dateComparator = (date1, date2) -> {
+			LocalDate ld1 = LocalDate.parse(date1, outputFormatter);
+			LocalDate ld2 = LocalDate.parse(date2, outputFormatter);
+			int yearComparison = Integer.compare(ld2.getYear(), ld1.getYear());
+			if (yearComparison != 0) {
+				return yearComparison;
+			}
+			int monthComparison = Integer.compare(ld2.getMonthValue(), ld1.getMonthValue());
+			if (monthComparison != 0) {
+				return monthComparison;
+			}
+			return Integer.compare(ld2.getDayOfMonth(), ld1.getDayOfMonth());
+		};
+
 		for (Cstatements statements : cstatementsList) {
-	        LocalDateTime localDateTime = statements.getCycledate().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+			LocalDateTime localDateTime = statements.getCycledate().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
 			LocalDate localDate = localDateTime.toLocalDate();
 			String outputDateStr = outputFormatter.format(localDate);
-			
+
 			cycledate.add(outputDateStr);
 		}
-		
-//		Collections.sort(cycledate);
-//
-//		for (String date : cycledate) {
-//            System.out.println(date);
-//        }
-//		 Arrays.sort(cycledate, Comparator.reverseOrder());
-//		Arrays.sort(cycledate,Comparator.reverseOrder());
-		 
+		Collections.sort(cycledate, dateComparator);
+
 		CardDetailsResponse c = new CardDetailsResponse();
 		c.setNumberx(caccounts.getNumberx());
 		c.setStgeneral(caccounts.getStgeneral());
