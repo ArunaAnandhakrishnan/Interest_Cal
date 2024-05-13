9a5346d74a69eb07cb277dec7149d8c6c811249b
Aruna ravichandran <V00554@VERINITE1.local>
2024-05-13 15:35:04 +0530

    three values work completed

--- Mad_Calculator/src/main/java/com/vernite/cal/service/AccountService.java
+++ Mad_Calculator/src/main/java/com/vernite/cal/service/AccountService.java
@@ -5,11 +5,9 @@ import java.util.List;
 
 import com.vernite.cal.model.Cstatements;
 import com.vernite.cal.model.Profiles;
-
 public interface AccountService {
 	
 
-	//public List<Cstatements> getStatement(String date) throws ParseException;
 	
 	
 
