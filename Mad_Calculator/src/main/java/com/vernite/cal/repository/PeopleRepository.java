aba14cca4227b6c1ffbfcae8223a584f51d859c9
Aruna ravichandran <V00554@VERINITE1.local>
2024-05-14 14:22:32 +0530

    changed added

--- Mad_Calculator/src/main/java/com/vernite/cal/repository/PeopleRepository.java
+++ Mad_Calculator/src/main/java/com/vernite/cal/repository/PeopleRepository.java
@@ -2,14 +2,18 @@ package com.vernite.cal.repository;
 
 import java.util.List;
 
+import com.vernite.cal.model.People;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.vernite.cal.model.People;
+
 
 @Repository
 public interface PeopleRepository extends JpaRepository<People, Long> {
 
-	People findByCustidnumber(String cusId);
+    @Query(value = "SELECT * FROM PEOPLE WHERE CUSTIDNUMBER =:cusId", nativeQuery = true)
+    People findValue(@Param("cusId") String cusId);
 
 }
